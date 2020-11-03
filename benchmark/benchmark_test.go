f19a8cb2883953a326a0d13a070db555b90d1b26
Peter Stefek <peterlikesemail@gmail.com>
2020-11-02 17:30:53 -0800

    fixed benchmarks

--- benchmark/benchmark_test.go
+++ benchmark/benchmark_test.go
@@ -18,40 +18,35 @@ func testBuild(input string) (*ast.Program, *object.Environment) {
 	return program, env
 }
 
-var result object.Object
-
 func BenchmarkFib(b *testing.B) {
-	var r object.Object
 	program, env := testBuild(`let fib = fn(x) { if (x == 1) { 1 } else { if (x ==0) { 1} else { fib(x - 1) + fib(x - 2) }}};fib(8)`)
 	for i := 0; i < b.N; i++ {
-		r = evaluator.Eval(program, env)
+		evaluator.Eval(program, env)
 	}
-	result = r
 }
 
 func BenchmarkPureFib(b *testing.B) {
-	var r object.Object
 	program, env := testBuild(`let fib = pfn(x) { if (x == 1) { 1 } else { if (x ==0) { 1} else { fib(x - 1) + fib(x - 2) }}};fib(8)`)
 	for i := 0; i < b.N; i++ {
-		r = evaluator.Eval(program, env)
+		evaluator.Eval(program, env)
 	}
-	result = r
 }
 
 func BenchmarkCollatz(b *testing.B) {
-	var r object.Object
-	program, env := testBuild(`let collatz = fn(n) { if (n==1) { 0 } else { if (n%2 == 0) { collatz(n/2) + 1 } else { collatz(3*n + 1) + 1 }}};let compute_sum_of_first_n_collatz = fn(n) { if (n== 1) { 0 } else { collatz(n) + compute_sum_of_first_n_collatz(n - 1) }}; compute_sum_of_first_n_collatz(100)`)
+	program, env := testBuild(`
+	let collatz = fn(n) { if (n==1) { 0 } else { if (n%2 == 0) { collatz(n/2) + 1 } else { collatz(3*n + 1) + 1 }}};
+	let compute_sum_of_first_n_collatz = fn(n) { if (n== 1) { 0 } else { collatz(n) + compute_sum_of_first_n_collatz(n - 1) }};
+	compute_sum_of_first_n_collatz(100)`)
 	for i := 0; i < b.N; i++ {
-		r = evaluator.Eval(program, env)
+		evaluator.Eval(program, env)
 	}
-	result = r
 }
 
 func BenchmarkPureCollatz(b *testing.B) {
-	var r object.Object
-	program, env := testBuild(`let collatz = pfn(n) { if (n==1) { 0 } else { if (n%2 == 0) { collatz(n/2) + 1 } else { collatz(3*n + 1) + 1 }}};let compute_sum_of_first_n_collatz = fn(n) { if (n== 1) { 0 } else { collatz(n) + compute_sum_of_first_n_collatz(n - 1) }}; compute_sum_of_first_n_collatz(100)`)
+	program, env := testBuild(`let collatz = pfn(n) { if (n==1) { 0 } else { if (n%2 == 0) { collatz(n/2) + 1 } else { collatz(3*n + 1) + 1 }}};
+	let compute_sum_of_first_n_collatz = fn(n) { if (n== 1) { 0 } else { collatz(n) + compute_sum_of_first_n_collatz(n - 1) }};
+	compute_sum_of_first_n_collatz(100)`)
 	for i := 0; i < b.N; i++ {
-		r = evaluator.Eval(program, env)
+		evaluator.Eval(program, env)
 	}
-	result = r
 }
